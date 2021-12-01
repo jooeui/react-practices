d4487728279a92432b0565d483a5dee025ebb6ef
jooeui <kje_0727@naver.com>
2021-12-01 15:32:27 +0900

    event #1

---
+++ 02.component/ex03/src/02/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import Titlebar01 from './TitleBar01';
+import Titlebar02 from './TitleBar02';
+
+export default function() {
+    return (
+        <Fragment>
+            <Titlebar01 />
+            <Titlebar02 />
+        </Fragment>
+    )
+}
