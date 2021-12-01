bca9f4020655026b0cba0eb6d709370f53814939
jooeui <kje_0727@naver.com>
2021-12-02 00:48:32 +0900

    제어 컴포넌트

--- 02.component/ex04/src/02/Form.js
+++ 02.component/ex04/src/02/Form.js
@@ -1,24 +1,49 @@
 import React, { useState } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCheckCircle, faTimesCircle} from '@fortawesome/free-solid-svg-icons';
 import './assets/Form.css';
 
 export default function Form() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [validEmail, setValidEmail] = useState(false);
 
+    const onChangeInputName = (e) => {
+        // setName(e.target.value);
+        // 10자 제한(validation)
+        setName(e.target.value.substr(0, 10));
+    };
+
+    const onChangeInputEmail = (e) => {
+        setEmail(e.target.value);
+
+        const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+        setValidEmail(re.test(e.target.value));
+    }
 
     return (
         <form id="joinForm" name="joinForm" method="post" action="/do/not/post">
             <label htmlFor="name">이름</label>
-            <input id="name" name="name" type="text" value={ "" } />
+            <input id="name" name="name" type="text" value={name} onChange={onChangeInputName}/>
 
             <label htmlFor="email">이메일</label>
-            <input id="email" name="email" type="text" value={ "" }/>
+            <input id="email" name="email" type="text" value={email} onChange={onChangeInputEmail}/>
+            {
+                email === '' ?
+                    null :
+                    validEmail ?
+                        <FontAwesomeIcon icon={faCheckCircle} style={ {marginLeft: 5, fontSize: 16, color: 'blue'} } /> :
+                        <FontAwesomeIcon icon={faTimesCircle} style={ {marginLeft: 5, fontSize: 16, color: 'red'} } />
+            }
+
 
             <label htmlFor="password">패스워드</label>
-            <input id="password" name="password" type="password" value={ "" } />
+            <input id="password" name="password" type="password" defaultValue={ "" } />
 
             <fieldset>
                 <legend>성별</legend>
-                <label>여</label> <input type="radio" name="gender" value={ "female" } defaultChecked={ true } />
-                <label>남</label> <input type="radio" name="gender" value={ "male" } defaultChecked={ false } />
+                <label>여</label> <input type="radio" name="gender" value={ "female" } defaultChecked={ true }/>
+                <label>남</label> <input type="radio" name="gender" value={ "male" } defaultChecked={ false }/>
             </fieldset>
 
             <label htmlFor="birthYear">생년</label>
@@ -33,7 +58,7 @@ export default function Form() {
             </select>
 
             <label htmlFor="birthYear">자기소개</label>
-            <textarea value={""} />
+            <textarea defaultValue={""} />
 
             <fieldset>
                 <legend>약관동의</legend>
