aaa12e590dcf1fc0351dd6b9b3fb650dbd7b89a6
jooeui <kje_0727@naver.com>
2021-12-01 16:41:35 +0900

    event #finish

--- 02.component/ex03/src/06/App.js
+++ 02.component/ex03/src/06/App.js
@@ -2,10 +2,17 @@ import React, { Component, useRef } from 'react';
 import './assets/scss/App.scss'
 
 export default class App extends Component {
+    onScroll(e) {
+        console.log(this.outterRef.scrollTop, " : ", this.outterRef.clientHeight, " : ", this.innerRef.clientHeight)
+    }
+
     render() {
         return(
-            <div className={'App'}>
-                <div ref={ (ref) => this.innerRef = ref }>
+            <div
+                ref={ref =>this.outterRef = ref } 
+                className={'App'}
+                onScroll={this.onScroll.bind(this)}>
+                <div ref={ ref => this.innerRef = ref }>
                     <ul>
                         {Array.from({length: 100}, (_, i) => i+1).map(i =>
                             <li key={i}>
